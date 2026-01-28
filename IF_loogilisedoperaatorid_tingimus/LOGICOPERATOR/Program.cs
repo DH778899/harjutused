c9384e934acf4057dbbbb88cddd4b92a854b0a24
Opilane TTHK <opilane@tthk.ee>
2026-01-28 09:38:53 +0200

    pooleldi lahendasin

--- IF_loogilisedoperaatorid_tingimus/LOGICOPERATOR/Program.cs
+++ IF_loogilisedoperaatorid_tingimus/LOGICOPERATOR/Program.cs
@@ -5,13 +5,24 @@
         static void Main(string[] args)
         {
             //tee neli bool-tüüpi muutujat,
+            bool pliiatsid = false;
+            bool paber = false;
+            bool lauad = false;
+            bool saag = false;
+
+            string sisestus = "";
 
             //nimedeks pane neile pliiatsid, paber, saag, lauad
 
             //küsi kasutajalt muutujasse "sisestus" vastus iga objekti
             //kohta küsimus "kas tal on X?" millele kasutaja saab vastata jah või ei
             //küsimine EI PEA olema tsüklis, võib ka teha 4 küsimust järjest
-
+            Console.WriteLine("kas teil on pliiatsid?");
+            if(sisestus == "jah")
+            {
+                pliiatsid = true;
+            }
+            else
             //kui ta on mingi objekti kohta vastanud ei, siis pane vastavasse muutujasse false
             //samamoodi kui ta on mingi objekti kohta vastanud jah, siis pane vastavasse muutujasse true
 
@@ -36,6 +47,27 @@
             //      |   jah     |   ei      |   ei      |   jah     ||  "sul on tööriistad, aga materjali mitte, ma ei oska öelda"
             //      |   ei      |   jah     |   jah     |   ei      ||  "sul on materjal, aga tööriistu mitte, mine k-rautasse"
             //      |  < kõik muud kombinatsoonid tingimustest >    ||  "tühjade kätega ei tee mitte midagi :c"
+            if (!pliiatsid && !paber && lauad && saag)
+            {
+                Console.WriteLine("võiksid õppida tisleriks");
+            }
+            else if (pliiatsid && paber && !lauad && !saag)
+            {
+                Console.WriteLine("võiksid õppida arhitektiks ja tisleriks, sul on kõik uksed avatud");
+            }
+           else if (pliiatsid && paber && lauad && saag)
+            {
+                Console.WriteLine("võiksid õppida arhitektiks ja tisleriks, sul on kõik uksed avatud");
+            }
+            else if (pliiatsid && !paber && !lauad && saag)
+                {
+                    Console.WriteLine("sul on tööriistad, aga materjali mitte, ma ei oska öelda");
+                }
+            else if (!pliiatsid && paber && lauad && !saag)
+            {
+                Console.WriteLine("sul on materjal, aga tööriistu mitte, mine k-rautasse");
+            }
+        }
         }
     }
-}
+
